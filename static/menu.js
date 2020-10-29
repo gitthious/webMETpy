d4e18b34d6ce64b1ef7a95248aac5df132c2dde2
Thierry Hervé <thierry.herve@masagroup.net>
2020-10-29 10:11:08 +0100

    le menu s'affiche correctement en fonction de la position cliquée

--- static/menu.js
+++ static/menu.js
@@ -15,6 +15,37 @@ var Menu = function() {
 			
 	//console.log("Menu", menu);
 
+	function affiche_menu(x, y) {
+		var f = d3.select("body").node();
+		menu.attr("class", "menu show-menu");
+		var h = f.clientHeight, // hauteur
+			l = f.clientWidth;  // largeur
+		var h2 = h/2, l2 = l/2;
+		
+		//console.log("affiche_mene", y, x, h, l);
+		if(y > h2 && x <= l2) { // cadran gauche/bas
+		  menu.style("left", x+"px");
+		  menu.style("bottom",(h-y)+"px");
+		  menu.style("right", "auto");
+		  menu.style("top", "auto");
+		} else if(y > h2 && x > l2) { // cadran droit/bas
+		  menu.style("right", (l-x)+"px");
+		  menu.style("bottom", (h-y)+"px");
+		  menu.style("left", "auto");
+		  menu.style("top", "auto");
+		} else if(y <= h2 && x <= l2) { // cadran gauche/haut
+		  menu.style("left", x+"px");
+		  menu.style("top", y+"px");
+		  menu.style("right", "auto");
+		  menu.style("bottom", "auto");
+		} else { // y <= h2 && x > l2 cadrant droit / haut
+		  menu.style("right", (l-x)+"px");
+		  menu.style("top", y+"px");
+		  menu.style("left", "auto");
+		  menu.style("bottom", "auto");
+		}
+	}
+	
 	this.show = function (event, items=["Aucune action définie"]) {
 		//console.log(event, items);
 		menu.selectAll("li")
@@ -25,18 +56,21 @@ var Menu = function() {
 					//console.log("click on menu item", this, d);
 					//selecteur.selection = d;
 					selecteur.action(event, d);
+					// si le menu est visible...
 					if(menu.attr("class") == "menu show-menu") {
+						// ... on l'efface
 						menu.attr("class", "menu");
 					}
 				})
 		
+		// si le menu est visible...
 		if(menu.attr("class") == "menu show-menu") {
+			// ... on l'efface
 			menu.attr("class", "menu");
 		}
 		else {
-			menu.attr("class", "menu show-menu");
-			menu.style("left", event.pageX+"px");
-			menu.style("top", event.pageY+"px");
+			// sinon on l'affiche à la position du curseur
+			affiche_menu(event.pageX, event.pageY);
 		}
 		event.stopPropagation();
 	}
