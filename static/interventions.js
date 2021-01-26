5801afd280092bc925212dbe775b3b1eb101e48b
Thierry Hervé <thierry.herve@masagroup.net>
2021-01-26 16:34:37 +0100

    tri des interventions en fonction de leur état en cours, terminées et en attente

--- static/interventions.js
+++ static/interventions.js
@@ -4,21 +4,73 @@ encoding utf-8
 */
 
 var VisuInterventions = function() {
+	var filtre = "pasfiltre";
 	
 	/* pour supprimer en cas de rechargement de la page 
 	   ou de relance du serveur web */
 	
-	d3.select("#interventions").select("ul").remove();
+	d3.select("#interventions").select("nav").remove();
 	
-	var elt = d3.select("#interventions").append("ul")
-	
-	this.update = function (){
+	var nav = d3.select("#interventions")
+		.append("nav")
+			.classed("interventions", true);
 		
+	nav.append("input")
+		.attr('type', "radio").attr('id', "pasfiltre")
+		.attr('name', "filtres").attr('value', "pasfiltre")
+		.attr('checked', "yes")
+		.on("click", function(){
+			filtre = "pasfiltre";
+			render();
+		});
+	nav.append("label")
+		.attr('for', "pasfiltre")
+		.html("Toutes");
+
+	nav.append("input")
+		.attr('type', "radio").attr('id', "encours")
+		.attr('name', "filtres").attr('value', "encours")
+		.on("click", function(){
+			filtre = "encours";
+			render();
+		});
+	nav.append("label")
+		.attr('for', "encours")
+		.html("En cours");
+
+	nav.append("input")
+		.attr('type', "radio").attr('id', "fin")
+		.attr('name', "filtres").attr('value', "fin")
+		.on("click", function(){
+			filtre = "fin";
+			render();
+		});
+	nav.append("label")
+		.attr('for', "fin")
+		.html("Terminées");
+
+	nav.append("input")
+		.attr('type', "radio").attr('id', "dmd")
+		.attr('name', "filtres").attr('value', "dmd")
+		.on("click", function(){
+			filtre = "dmd";
+			render();
+		});
+	nav.append("label")
+		.attr('for', "dmd")
+		.html("En attentes");
+				
+	var elt = nav.append('ul');
+
+	var interventions = [];
+
+	this.update = function (){
+		if(!selecteur) return;
 		// reorg events
 		//console.log("VisuInterventions.update",data.events)
 		var evts = data.events;
 		if(!evts){ return; }
-		var interventions = [];
+		interventions = [];
 		/*
 			Une intervention est identifiée avec les champs agent, intervention, args, donc sans l'heure donc une par ligne.
 			Les "encours" sont ajoutés à chaque intervention "début".
@@ -48,9 +100,30 @@ var VisuInterventions = function() {
 			}
 		}
 		
+		render();
+	}
+	
+	function a_afficher(intervention){
+		encours = intervention[4];
+		if(filtre == "encours"){
+			if(encours.length == 0) return true;
+			if(encours[0].search("fin") != -1) return false;
+		} else if(filtre=="fin") {
+			if(encours.length == 0) return false;
+			if(encours[0].search("fin") != -1) return true;
+			return false;
+		} else if(filtre=="dmd") {
+			if(encours.length == 0) return false;
+			if(encours[0].search("dmd") != -1) return true;
+			return false;
+		}
+		return true;
+	}
+	
+	function render() {
 		//console.log("VisuInterventions.update",interventions)
 		elt.selectAll('li')
-			.data(interventions, d => d)
+			.data(interventions.filter(i => a_afficher(i)), d => d)
 			.join('li')
 				.classed('intervention', true)
 				.classed('finie', function (d){
@@ -79,6 +152,10 @@ var VisuInterventions = function() {
 						.join('li')
 							.classed('encours', true)
 							.text(d => d)
+							// ça ne fonctionne pas car on ne manipule pas un "type selectionnable"
+							// on abandonne pour l'instant
+							// mais le mouseenter et leave fonctionnent
+							//.call(selecteur.on("AgentExploitation", "interventions"));
 	}
 
 	this.update();
